086f332d5fbefc447e3fba35a19f0bcc998d619b
Kitaka17 <jjbkitaka@gmail.com>
2023-03-17 09:14:52 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a' ; i <= '2' ; i++)
+		putchar('\n');
+	return (0);
+}
